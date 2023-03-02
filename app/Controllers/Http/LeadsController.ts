a631f54ef2c576765f4ffdec4c38459120ac0b9a
JadyelGomes <jadyel.gomes@opportunatecnologia.com.br>
2023-03-02 10:41:50 -0300

    Modelo leads criado

--- app/Controllers/Http/LeadsController.ts
+++ app/Controllers/Http/LeadsController.ts
@@ -1,14 +1,12 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Database from '@ioc:Adonis/Lucid/Database'
+import TbLeads from 'App/Models/TbLeads';
 
 export default class LeadsController {
     
     async index({ request, response }: HttpContextContract) {
 
         try {
-            const leads = await Database
-                .from('tb_leads') // 👈 gives an instance of select query builder
-                .select('*');
+            const leads = await TbLeads.all()
             return leads;
         } catch (error) {
             return response.unauthorized(error.message)
